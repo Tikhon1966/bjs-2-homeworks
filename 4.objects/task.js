3a6ed763fb8d1e780582fc36724bc895365b8c94
Oleg <ovt1966@gmail.com>
2023-09-07 18:59:36 +0300

    Решение 4 задания

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,28 @@
 function Student(name, gender, age) {
-  
-}
-
-Student.prototype.setSubject = function (subjectName) {
-  
-}
-
-Student.prototype.addMarks = function (...marks) {
-  
-}
-
-Student.prototype.getAverage = function () {
-  
-}
-
-Student.prototype.exclude = function (reason) {
-  
+	this.name = name;
+	this.gender = gender;
+	this.age = age;
+	this.marks = [];
+	this.setSubject = function(subjectName) {
+		this.subject = subjectName;
+	}
+	this.addMarks = function(...marksToAdd) {
+		if (!this.marks) {
+			console.log("Error: Student has been expelled.");
+			return;
+		}
+		this.marks.push(...marksToAdd);
+	};
+	this.getAverage = function() {
+		if (!this.marks || !this.marks.length) {
+			return 0;
+		}
+		const sum = this.marks.reduce((acc, curr) => acc + curr);
+		return sum / this.marks.length;
+	};
+	this.exclude = function(reason) {
+		delete this.subject;
+		delete this.marks;
+		this.excluded = reason;
+	};
 }
